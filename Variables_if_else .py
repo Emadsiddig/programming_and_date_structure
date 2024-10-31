59a2a18fe2a538378b9a206fa0eb31b081d4681c
emad <emadsiddig72@gmail.com>
2024-10-31 23:52:40 +0200

    Variabless_if_else.py
    lap2

---
+++ Variables_if_else .py
@@ -0,0 +1,8 @@
+#to know the sign of number
+nu = int(input("the number is :"))
+if nu > 0:
+    print("the number is positive")
+elif nu < 0 :
+    print("the number is positive ")
+else :
+    print("the number is zero")
